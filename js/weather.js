dd07a7c2d625752dc6536abdec9eeef88bd7cbeb
Una <contrarian33@gmail.com>
2022-04-24 23:23:40 +0900

    first commit

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "b32b619615fbc6be1d847f024166c5f5";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.main.temp}°C / ${data.weather[0].main}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
